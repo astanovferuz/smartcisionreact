b0cec3191da34819f8b3831daa4f82050effdc17
Feruz <astanovferuz@gmail.com>
2020-10-07 22:50:51 +0800

    Updates

---
+++ precache-manifest.5ae9c362b0a5734b7dac649f531980d9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65fa5ef5085b6b989401d535566ddb9a",
+    "url": "/smartcisionappreact/index.html"
+  },
+  {
+    "revision": "1ec300ef22410fed5ef8",
+    "url": "/smartcisionappreact/static/css/2.f5ae846d.chunk.css"
+  },
+  {
+    "revision": "82763225cec4b89aabd6",
+    "url": "/smartcisionappreact/static/css/main.90af9f16.chunk.css"
+  },
+  {
+    "revision": "1ec300ef22410fed5ef8",
+    "url": "/smartcisionappreact/static/js/2.c6cac0fb.chunk.js"
+  },
+  {
+    "revision": "f6a33b08b8dab27dd8ab525dc9994832",
+    "url": "/smartcisionappreact/static/js/2.c6cac0fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82763225cec4b89aabd6",
+    "url": "/smartcisionappreact/static/js/main.a561504b.chunk.js"
+  },
+  {
+    "revision": "5ecea9e2f69825427f2a",
+    "url": "/smartcisionappreact/static/js/runtime-main.ea2f3693.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/smartcisionappreact/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/smartcisionappreact/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/smartcisionappreact/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/smartcisionappreact/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/smartcisionappreact/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
